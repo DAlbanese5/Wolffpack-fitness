bf764eecf8a6807c04dfbaf0e666524fc073118b
Madi Bromfield <madibromfield215@gmail.com>
2024-10-09 12:29:06 -0500

    navbar and backgorund

--- myapp-backend/myapp-frontend/src/App.jsx
+++ myapp-backend/myapp-frontend/src/App.jsx
@@ -1,18 +1,9 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/Navbar";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  );
+  return <NavBar />;
 }
 
 export default App;
