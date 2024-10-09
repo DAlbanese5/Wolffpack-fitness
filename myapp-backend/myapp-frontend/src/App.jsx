0daa1072d85040e972024504049c235fa24b9b41
Daulton Albanese <daultonalbanese5@gmail.com>
2024-10-09 14:07:26 -0400

    update2

--- myapp-backend/myapp-frontend/src/App.jsx
+++ myapp-backend/myapp-frontend/src/App.jsx
@@ -1,11 +1,24 @@
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/Navbar";
+import AboutMe from "./components/aboutMe"; // Ensure you import the AboutMe component
 import "./App.css";
+import Home from "./components/home";
 
 function App() {
-  return <NavBar />;
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/about" element={<AboutMe />} />
+        {/* Add more routes here as needed */}
+        <Route path="/" element={<Home />} /> 
+      </Routes>
+    </div>
+  );
 }
 
-
-
 export default App;
+
+
+
+
--- myapp-backend/myapp-frontend/src/components/Navbar.jsx
+++ myapp-backend/myapp-frontend/src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <div className="navbar">
-        <button>Product</button>
-        <button>Workouts</button>
-        <button>About Me</button>
-        <button>Contact</button>
+        {/* <Link to="/product">
+          <button>Product</button>
+        </Link>
+        <Link to="/workouts">
+          <button>Workouts</button>
+        </Link> */}
+        <Link to="/about">
+          <button>About Me</button>
+        </Link>
+        {/* <Link to="/contact">
+          <button>Contact</button>
+        </Link> */}
       </div>
       <button className="cart-button">Cart</button>{" "}
       {/* Cart button on the right */}
