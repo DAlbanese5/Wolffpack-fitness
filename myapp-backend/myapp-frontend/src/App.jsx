bcb1f2fece85d6db75cb297b62f554e35b097b86
Daulton Albanese <daultonalbanese5@gmail.com>
2024-10-09 13:46:00 -0400

    about

--- myapp-backend/myapp-frontend/src/App.jsx
+++ myapp-backend/myapp-frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return <NavBar />;
 }
+
+
 
 export default App;
---
+++ myapp-backend/myapp-frontend/src/components/aboutMe.jsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p> hello</p>
+      <p>Here a bit about my background and interests.</p>
+      <ul>
+        <li>Hobby or interest 1</li>
+        <li>Hobby or interest 2</li>
+        <li>Hobby or interest 3</li>
+      </ul>
+    </div>
+  );
+};
+
+
+export default AboutMe;
