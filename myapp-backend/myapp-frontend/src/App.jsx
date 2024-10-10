b795e60fc07327c0192f905a64660fcb1ed46267
Madi Bromfield <madibromfield215@gmail.com>
2024-10-09 23:41:51 -0500

    navbar buttons

--- myapp-backend/myapp-frontend/src/App.jsx
+++ myapp-backend/myapp-frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/Navbar";
 import AboutMe from "./components/aboutMe"; // Ensure you import the AboutMe component
-import "./App.css";
 import Home from "./components/home";
+import Workouts from "./components/workouts";
+import Products from "./components/products";
+import ContactMe from "./components/contactMe";
+import "./App.css";
 
 function App() {
   return (
@@ -10,15 +13,14 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/about" element={<AboutMe />} />
+        <Route path="/workouts" element={<Workouts />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<ContactMe />} />
         {/* Add more routes here as needed */}
-        <Route path="/" element={<Home />} /> 
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
-
-
-
-
