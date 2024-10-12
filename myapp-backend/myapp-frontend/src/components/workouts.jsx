2964555cd941c824b48d03c9181ea35997681217
Daulton Albanese <daultonalbanese5@gmail.com>
2024-10-12 01:52:06 -0400

    e book descriptions

--- myapp-backend/myapp-frontend/src/components/workouts.jsx
+++ myapp-backend/myapp-frontend/src/components/workouts.jsx
@@ -6,14 +6,14 @@ const Workouts = () => {
     <img src="images/glutesonly1.png" alt="Glute only image" id="glutepic"></img>
     <div className="eBook1">
     <a href="https://payhip.com/b/Ek9pN">Glute Only Guide</a>
-    <p>Put a brief description here.....</p>
+    <p>Anyone who wants to tighten up those glutes and get that booty lookin good, this one is for you!</p>
     </div>
     <br />
     <h2 className="ebooktitles">30 Mins or Less E-Book</h2> 
     <img src="images/30mins.png" alt="30 mins or less image" id="ebook2pic"></img>
     <div className="eBook2">
       <a href="https://payhip.com/b/Mt8D0\">30 Minutes or Less</a>
-      <p>Put a brief description here.....</p>
+      <p>A time-efficient exercise routine to engage multiple muscle groups and improve overall fitness designed for people who are serious about their fitness but might be on a time crunch! Maybe it is on a lunch break or before the kids wake up for school. Whatever it is, in 30 minutes, this E-Book will have you feeling good!</p>
       </div>
   </div>;
 };
