647ddb2aadc9b8302f66392ddf4d2735e90d3b45
Daulton Albanese <daultonalbanese5@gmail.com>
2024-10-12 01:38:07 -0400

    ebooks link added

--- myapp-backend/myapp-frontend/src/components/workouts.jsx
+++ myapp-backend/myapp-frontend/src/components/workouts.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Workouts = () => {
-  return <div></div>;
+  return <div className="workoutcontainer">
+    <h2 className="ebooktitles">Glute Only Guide</h2>
+    <img src="images/glutesonly1.png" alt="Glute only image" id="glutepic"></img>
+    <div className="eBook1">
+    <a href="https://payhip.com/b/Ek9pN">Glute Only Guide</a>
+    <p>Put a brief description here.....</p>
+    </div>
+    <br />
+    <h2 className="ebooktitles">30 Mins or Less E-Book</h2> 
+    <img src="images/30mins.png" alt="30 mins or less image" id="ebook2pic"></img>
+    <div className="eBook2">
+      <a href="https://payhip.com/b/Mt8D0\">30 Minutes or Less</a>
+      <p>Put a brief description here.....</p>
+      </div>
+  </div>;
 };
 
 export default Workouts;
